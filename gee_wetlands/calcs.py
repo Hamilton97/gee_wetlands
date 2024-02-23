f7405c7d1eed2218222a4d2ebf79e9493a209509
Ryan Hamilton <ryangilberthamilton@gmail.com>
2024-02-23 11:49:55 -0500

    Implement todo: ndvi calculator

---
+++ gee_wetlands/calcs.py
@@ -0,0 +1,17 @@
+"""
+TODO: Implement all standard calcs as helper functions
+    - NDVI
+    - SAVI
+    - Tasseled Cap
+    - Ratio
+    - Phase
+    - Amplitude
+"""
+from typing import Callable
+import ee
+
+
+def compute_ndvi(nir: str, red: str) -> Callable:
+    def wrapper(img: ee.Image) -> ee.Image:
+        return img.normalizedDifference([nir, red]).rename('NDVI')
+    return wrapper
