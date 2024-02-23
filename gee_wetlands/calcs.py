48f534c2530e415e5e27235a2dca1f9a79c46b66
Ryan Hamilton <ryangilberthamilton@gmail.com>
2024-02-23 12:21:07 -0500

    Add compute_ndvi function to calculate NDVI

--- gee_wetlands/calcs.py
+++ gee_wetlands/calcs.py
@@ -12,6 +12,16 @@ import ee
 
 
 def compute_ndvi(nir: str, red: str) -> Callable:
+    """
+    Computes the Normalized Difference Vegetation Index (NDVI) for an image.
+    
+    Args:
+        nir (str): The name of the near-infrared band.
+        red (str): The name of the red band.
+    
+    Returns:
+        Callable: A function that takes an ee.Image as input and returns an ee.Image with the NDVI band added.
+    """
     def wrapper(img: ee.Image) -> ee.Image:
         return img.addBands(img.normalizedDifference([nir, red]).rename('NDVI'))
     return wrapper
